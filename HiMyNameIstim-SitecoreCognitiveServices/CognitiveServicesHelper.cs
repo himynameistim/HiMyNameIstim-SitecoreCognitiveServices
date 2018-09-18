389c9c570ba642fa85d34c5933f1cbe1b30654b6
timgriff84 <5212530+timgriff84@users.noreply.github.com>
2018-09-18 16:26:12 +0100

    Sitecore save action

---
+++ HiMyNameIstim-SitecoreCognitiveServices/CognitiveServicesHelper.cs
@@ -0,0 +1,70 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HiMyNameIstim_SitecoreCognitiveServices
+{
+    public class CognitiveServicesHelper
+    {
+        //TODO: Get <Subscription Key> from Sitecore settings item.
+        const string subscriptionKey = "b0e316ed75164b9da70aa01f35b65b89";
+
+        //TODO: Get uri base from Sitecore settings item
+        const string uriBase = "https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/analyze";
+
+        /// <summary>
+        /// Gets the analysis of the specified image file by using
+        /// the Computer Vision REST API.
+        /// </summary>
+        /// <param name="imageFilePath">The image file to analyze.</param>
+        public static JToken MakeAnalysisRequest(byte[] imageByteData)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+
+                // Request headers.
+                client.DefaultRequestHeaders.Add(
+                    "Ocp-Apim-Subscription-Key", subscriptionKey);
+
+                // Request parameters. A third optional parameter is "details".
+                string requestParameters =
+                    "visualFeatures=Categories,Description,Color";
+
+                // Assemble the URI for the REST API Call.
+                string uri = uriBase + "?" + requestParameters;
+
+                HttpResponseMessage response;
+
+                using (ByteArrayContent content = new ByteArrayContent(imageByteData))
+                {
+                    // This example uses content type "application/octet-stream".
+                    // The other content types you can use are "application/json"
+                    // and "multipart/form-data".
+                    content.Headers.ContentType =
+                        new MediaTypeHeaderValue("application/octet-stream");
+
+                    // Make the REST API call.
+                    response = client.PostAsync(uri, content).Result;
+                }
+
+                // Get the JSON response.
+                string contentString = response.Content.ReadAsStringAsync().Result;
+
+                // Return the results
+                return JToken.Parse(contentString).ToString();
+            }
+            catch (Exception e)
+            {
+                //TODO: Add exception logging
+
+                throw;
+            }
+        }
+    }
+}
---
+++ HiMyNameIstim-SitecoreCognitiveServices/ImageSavedHandler.cs
@@ -0,0 +1,64 @@
+﻿using Sitecore.Data;
+using Sitecore.Data.Fields;
+using Sitecore.Data.Items;
+using Sitecore.Events;
+using Sitecore.Resources.Media;
+using Sitecore.SecurityModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HiMyNameIstim_SitecoreCognitiveServices
+{
+    public class ImageSavedHandler
+    {
+        public void OnItemSaved(object sender, EventArgs args)
+        {
+            // Extract the item from the event Arguments
+            Item savedItem = Event.ExtractParameter(args, 0) as Item;
+
+            // Allow only non null items and allow only items from the master database
+            if (savedItem != null && savedItem.Database.Name.ToLower() == "master")
+            {
+                // Limit templates to Jpeg
+
+                if (savedItem.TemplateID == ID.Parse("{F1828A2C-7E5D-4BBD-98CA-320474871548}"))
+                {
+                    Media media = MediaManager.GetMedia(savedItem);
+                    if (media != null)
+                    {
+                        MediaStream mediaStream = media.GetStream();
+                        Byte[] bytes = new Byte[mediaStream.Length];
+                        mediaStream.Stream.Read(bytes, 0, bytes.Length);
+
+                        var analysisResults = CognitiveServicesHelper.MakeAnalysisRequest(bytes);
+
+
+                        // Get the data that you need to populate here
+                        //ImageField imageField = savedItem.Fields["Media"];
+                        //MediaItem mediaItem = imageField.MediaItem;
+                        //Stream stream = mediaItem.GetMediaStream();
+                        //Byte[] bytes = new Byte[stream.Length];
+                        //stream.Read(bytes, 0, bytes.Length);
+
+                        //var analysisResults = CognitiveServicesHelper.MakeAnalysisRequest(bytes);
+
+                        // Start Editing the Item
+
+                        using (new SecurityDisabler())
+                        {
+                            savedItem.Editing.BeginEdit();
+
+                            var result = analysisResults;
+
+                            savedItem.Editing.EndEdit();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
